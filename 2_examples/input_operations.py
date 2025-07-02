5635aa79d7f66b9d40d1c897a5cf6b89313fc9e5
Mudit Sapra <codesmudit@gmail.com>
2025-07-02 17:41:36 +0530

    Create input_operations.py

---
+++ 2_examples/input_operations.py
@@ -0,0 +1,17 @@
+a = int(input("Please enter your first number : "))
+b = int(input("Please enter your second number : "))
+sum = a + b
+print("The sum of the given numbers is : ",sum)
+
+side = int(input("Please enter the length of the side of square : "))
+Area = side*side
+print("The area of the square is : ",Area)
+
+num1 = float(input("Please enter the first number : "))
+num2 = float(input("Please enter the second number : "))
+average = (num1+num2)/2
+print("The average of the two numbers is : ",average)
+
+m = int(input("Please enter the first number : "))
+n = int(input("Please enter the second number : "))
+print(m>=n)
