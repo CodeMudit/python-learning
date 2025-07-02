9e8efcc8f3d65561476b7a156b9f307dd7c240f4
Mudit Sapra <codesmudit@gmail.com>
2025-07-02 17:34:00 +0530

    Create datatypes.py

---
+++ 1_basics/datatypes.py
@@ -0,0 +1,19 @@
+name = "Mudit"
+age = 17
+price = 25.98
+old = True
+a = None
+
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(a))
+
+m = 3
+n = 2
+sum = m+n
+difference = m-n
+print(sum)
+print(difference)
